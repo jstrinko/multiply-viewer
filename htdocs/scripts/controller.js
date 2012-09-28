ac1f57c060c3f2011fcb10810b5edd70eeb7cad9
Jeff Strinko <namesty@loonix.(none)>
2012-09-28 19:24:43 -0400

    Updates

--- htdocs/scripts/controller.js
+++ htdocs/scripts/controller.js
@@ -1,12 +1,31 @@
-var App = {
-    cache: {},
+var Client = function() {
+    this.cache = {};
+    this.initialize = function() {
+	this.router = new App_Router();
+	this.router.set_app(this);
+	this.socket_server = "http://" + GLOBAL_hostname + ":" + GLOBAL_config.socket_port;
+	this.socket = io.connect(this.socket_server);
+	this.init_socket();
+    };
+    this.init_socket = function() {
+	var client = this;
+	this.socket.on('connect', function() {
+	    client.socket.on('response', function(data) {
+		client.handle_response(data);
+	    });
+	    Backbone.history.start();
+	    client.router.navigate();
+	});
+    };
+    this.handle_response = function(data) {
+	$('body').html(JSON.stringify(data));
+    };
 };
 
+var Single_Client = new Client();
+
 function main() {
-    App.router = new App_Router();
-    App.router.set_app(App);
-    Backbone.history.start();
-    App.router.navigate();
+    Single_Client.initialize();
 }
 
 var App_Router = Backbone.Router.extend({
@@ -17,7 +36,7 @@ var App_Router = Backbone.Router.extend({
 	var body_view = new Body_View({ model: new Body() });
     },
     show_main: function() {
-	
+	this.app.socket.emit('request', { "uri": "/api/user" });
     },
     set_app: function(app) {
 	this.app = app;
--- streaming.js
+++ streaming.js
@@ -1,5 +1,7 @@
 var Socket_Server = require('./lib/socket-server').Socket_Server;
+var Multiply_Handler = require('./lib/multiply-handler').Multiply_Handler;
 var server = new Socket_Server();
+server.api_handler = new Multiply_Handler();
 server.initialize('config.json');
 server.start();
 
